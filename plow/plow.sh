3cb855511068fc0df9fe167573685bcbe2499fb3
mrmiguez <r.m.miguez@gmail.com>
2015-05-29 15:12:16 -0400

    absolute paths, more stats in reports, bug fixes for other machines

--- plow/plow.sh
+++ plow/plow.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 # Ready the outputs
-rm -rf ./harvest
-mkdir harvest
-rm assets/setSpec.txt
-rm assets/setName.txt
+rm -rf /home/mmiguez/bin/plow/harvest
+mkdir /home/mmiguez/bin/plow/harvest
+rm /home/mmiguez/bin/plow/assets/setSpec.txt
+rm /home/mmiguez/bin/plow/assets/setName.txt
 
 #build arrays
-touch assets/setSpec.txt
-touch assets/setName.txt
-python assets/setSpecFetch.py
+touch /home/mmiguez/bin/plow/assets/setSpec.txt
+touch /home/mmiguez/bin/plow/assets/setName.txt
+python /home/mmiguez/bin/plow/assets/setSpecFetch.py
 
-printf 'setList=(' > assets/setSpec.txt
+printf 'setList=(' > /home/mmiguez/bin/plow/assets/setSpec.txt
 for setSpec in $( xmlstarlet sel -T -t -v //setSpec assets/setSpec.xml ); do
-	printf '%s ' $setSpec >> assets/setSpec.txt
+	printf '%s ' $setSpec >> /home/mmiguez/bin/plow/assets/setSpec.txt
 done
-printf ')' >> assets/setSpec.txt
+printf ')' >> /home/mmiguez/bin/plow/assets/setSpec.txt
 
-printf 'setName=(' > assets/setName.txt
+printf 'setName=(' > /home/mmiguez/bin/plow/assets/setName.txt
 for setName in $( xmlstarlet sel -T -t -v //setName assets/setSpec.xml ); do
-	printf '%s ' $setName >> assets/setName.txt
+	printf '%s ' $setName >> /home/mmiguez/bin/plow/assets/setName.txt
 done
-printf ')' >> assets/setName.txt
+printf ')' >> /home/mmiguez/bin/plow/assets/setName.txt
 
 # Define report functions
 breaker () {
-	python ~/bin/dc_breaker/dc_breaker.py -e $1 $2 | wc -l
+	python /home/mmiguez/bin/dc_breaker/dc_breaker.py -e $1 $2 | wc -l
 }
 count () {
 	xmlstarlet el $1 | grep $2 | wc -l
@@ -33,39 +33,43 @@ count () {
 
 
 # Reading set data into the array
-source assets/setSpec.txt
-source assets/setName.txt
+source /home/mmiguez/bin/plow/assets/setSpec.txt
+source /home/mmiguez/bin/plow/assets/setName.txt
 iso=`date -I`
 mark=0
 
 # Setting up the loop
 for i in ${setList[@]}; do
 	# Set up the harvest
-	python ~/bin/pyoaiharvester/pyoaiharvest.py -l http://fsu.digital.flvc.org/oai2 -s $i -o harvest/$i$iso.xml
+	python /home/mmiguez/bin/pyoaiharvester/pyoaiharvest.py -l http://fsu.digital.flvc.org/oai2 -s $i -o harvest/$i$iso.xml
 done
 printf "\n\nHarvest complete.\n\n"
 
 # Start report
 touch fsudlReport$iso.csv
-echo 'setSpec, # of records, # of titles, # of creators, avg creators per record, # of subjects, avg subjects per record' >> fsudlReport$iso.csv
+echo 'setName, setSpec, # of records, # of titles, # of creators, avg creators per record, # of dates, # of coverages, # of formats, # of types, # of subjects, avg subjects per record' >>fsudlReport$iso.csv
 
 # Setting up the report loop
 for i in ${setList[@]}; do
 	echo "Analysing $i"
 	setName=${setName[$mark]}
-	recNum=`count ./harvest/$i* record$`
+	recNum=`count /home/mmiguez/bin/plow/harvest/$i* record$`
 	if [ $recNum -eq 0 ]; then
-		printf '%s, 0, 0, 0, 0, 0, 0\n' $i >> fsudlReport$iso.csv
+		printf '%s, 0, 0, 0, 0, 0, 0\n' $i >> /home/mmiguez/bin/plow/fsudlReport$iso.csv
 	else
-		titleNum=`breaker title ./harvest/$i*`
-		creatorNum=`breaker creator ./harvest/$i*`
-		subNum=`breaker subject ./harvest/$i*`
-		printf '%s, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f\n' $i $recNum $titleNum $creatorNum $(( $creatorNum / $recNum )) $subNum $(( $subNum / $recNum )) >> fsudlReport$iso.csv
+		titleNum=`breaker title /home/mmiguez/bin/plow/harvest/$i*`
+		creatorNum=`breaker creator /home/mmiguez/bin/plow/harvest/$i*`
+		dateNum=`breaker date /home/mmiguez/bin/plow/harvest/$i*`
+		coverNum=`breaker coverage /home/mmiguez/bin/plow/harvest/$i*`
+		formatNum=`breaker format /home/mmiguez/bin/plow/harvest/$i*`
+		typeNum=`breaker type /home/mmiguez/bin/plow/harvest/$i*`
+		subNum=`breaker subject /home/mmiguez/bin/plow/harvest/$i*`
+		printf '%s, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f\n' $i $recNum $titleNum $creatorNum $(( $creatorNum / $recNum )) $dateNum $coverNum $formatNum $typeNum $subNum $(( $subNum / $recNum )) >> /home/mmiguez/bin/plow/fsudlReport$iso.csv
 	fi
 	mark=$(( $mark + 1 ))
 done
 printf "\nReport filed.\n\n"
 
 # archive harvest
-tar cvf - ./harvest/* | gzip > fsudlharvest$iso.tar.gz
+tar cvf - /home/mmiguez/bin/plow/harvest/* | gzip > /home/mmiguez/bin/plow/fsudlharvest$iso.tar.gz
 printf "\nResults archived.\n\n"
--- plow/plowReport.sh
+++ plow/plowReport.sh
@@ -2,7 +2,7 @@
 
 # Define report functions
 breaker () {
-	python ~/bin/dc_breaker/dc_breaker.py -e $1 $2 | wc -l
+	python /home/mmiguez/bin/dc_breaker/dc_breaker.py -e $1 $2 | wc -l
 }
 # Define counting function
 count () {
@@ -11,27 +11,31 @@ count () {
 
 # Read date and start report
 iso=`date -I`
-touch fsudlReport$iso.csv
+touch /home/mmiguez/bin/plow/fsudlReport$iso.csv
 
 # Read set list into array
-source assets/setSpec.txt
-source assets/setName.txt
+source /home/mmiguez/bin/plow/assets/setSpec.txt
+source /home/mmiguez/bin/plow/assets/setName.txt
 mark=0
 
-echo 'setSpec, # of records, # of titles, # of creators, avg creators per record, # of dates, # of coverages, # of formats, # of types, # of subjects, avg subjects per record' >> fsudlReport$iso.csv
+echo 'setSpec, # of records, # of titles, # of creators, avg creators per record, # of dates, # of coverages, # of formats, # of types, # of subjects, avg subjects per record' >> /home/mmiguez/bin/plow/fsudlReport$iso.csv
 
 # Setting up the report loop
 for i in ${setList[@]}; do
 	echo "Analysing $i"
 	setName=${setName[$mark]}
-	recNum=`count ./harvest/$i* record$`
+	recNum=`count /home/mmiguez/bin/plow/harvest/$i* record$`
 	if [ $recNum -eq 0 ]; then
-		printf '%s, 0, 0, 0, 0, 0, 0\n' $i >> fsudlReport$iso.csv
+		printf '%s, 0, 0, 0, 0, 0, 0\n' $i >> /home/mmiguez/bin/plow/fsudlReport$iso.csv
 	else
-		titleNum=`breaker title ./harvest/$i*`
-		creatorNum=`breaker creator ./harvest/$i*`
-		subNum=`breaker subject ./harvest/$i*`
-		printf '%s, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f\n' $i $recNum $titleNum $creatorNum $(( $creatorNum / $recNum )) $subNum $(( $subNum / $recNum )) >> fsudlReport$iso.csv
+		titleNum=`breaker title /home/mmiguez/bin/plow/harvest/$i*`
+		creatorNum=`breaker creator /home/mmiguez/bin/plow/harvest/$i*`
+		dateNum=`breaker date /home/mmiguez/bin/plow/harvest/$i*`
+		coverNum=`breaker coverage /home/mmiguez/bin/plow/harvest/$i*`
+		formatNum=`breaker format /home/mmiguez/bin/plow/harvest/$i*`
+		typeNum=`breaker type /home/mmiguez/bin/plow/harvest/$i*`
+		subNum=`breaker subject /home/mmiguez/bin/plow/harvest/$i*`
+		printf '%s, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f\n' $i $recNum $titleNum $creatorNum $(( $creatorNum / $recNum )) $subNum $(( $subNum / $recNum )) >> /home/mmiguez/bin/plow/fsudlReport$iso.csv
 	fi
 	mark=$(( $mark + 1 ))
 done
