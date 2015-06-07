7e41112cc3a53ded76852cbfac12cc0db02ba6f6
mrmiguez <r.m.miguez@gmail.com>
2015-06-07 09:16:25 -0400

    semicolon field separation

--- csv4cat/csv4cat.py
+++ csv4cat/csv4cat.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 def writeCSV(fileName):
-  header = ['Identifiers', 'Title', 'Date', 'Creator']
+  header = ['Identifiers;', 'Title;', 'Creator;', 'Date;']
   NS = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'dc': 'http://purl.org/dc/elements/1.1/'}
   with open(fileName + '.csv', 'w') as f:
     writer = csv.writer(f)
@@ -15,12 +15,16 @@ def writeCSV(fileName):
       data = []
       for identifier in record.iterfind('.//{%s}identifier' % NS['dc']):
         data.append(identifier.text)
+      data.append(';')
       for title in record.iterfind('.//{%s}title' % NS['dc']):
         data.append(title.text)
+      data.append(';')        
       for creator in record.iterfind('.//{%s}creator' % NS['dc']):
         data.append(creator.text)
+      data.append(';')      
       for date in record.iterfind('.//{%s}date' % NS['dc']):
-        data.append(date.text)        
+        data.append(date.text)
+      data.append(';')        
       writer.writerow(data)
 
 name = os.path.splitext(sys.argv[1])[0]
