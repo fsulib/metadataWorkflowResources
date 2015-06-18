74e7bdbe62ab94c3198d363c80384c13824a1cd7
mrmiguez <r.m.miguez@gmail.com>
2015-06-18 10:11:43 -0400

    namespacing the better way

--- MODSbuilder/MODSbuilder-dev.py
+++ MODSbuilder/MODSbuilder-dev.py
@@ -3,20 +3,17 @@ import csv
 import sys
 import os
 
-NS = {None:'http://www.loc.gov/mods/v3', 'flvc':'info:flvc/manifest/v1'}
-
-#def makeMODS(tag, text):
-  
+modsNS = 'http://www.loc.gov/mods/v3'
+flvcNS = 'info:flvc/manifest/v1'
+NSmap = {None: modsNS, 'flvc': flvcNS}
 
 def buildRecord(record):
-  root = etree.Element('mods', nsmap=None, xsi:SchemaLocation='http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-4.xsd', version='3.4' )
+  root = etree.Element('root', nsmap = NSmap)
   for tag, text in record.items():
-#    tag = 
-#    print(path, text)
-    child = etree.SubElement(root, tag)
-    child.text = text
+    elem = etree.SubElement(root, tag)
+    elem.text = text
   xmlString = etree.tostring(root, pretty_print=True)
-  f = open('output/%s.xml' % record['IID'], 'w')
+  f = open('output/%s.xml' % record['identifier'], 'w')
   f.write(xmlString.decode('utf-8'))
   f.close()
 
@@ -24,24 +21,7 @@ def readCSV(fileIn):
   with open(fileIn) as csvfile:
     source = csv.DictReader(csvfile, delimiter=';', quotechar='"')
     for row in source:
-#      print('%s' % row['IID'])
-#      for key, value in row.items():
       buildRecord(row)  
-
 
 name = sys.argv[1]
 readCSV(name)
-
-#DICTIONARY METHODS
-#Dictionary.keys() --> gets list of keys
-#for key, value in Dictionary.items():
-#  print(key, value)   
-#----------------------> iterate over dict contents
-
-#BUILDING XML
-#root = etree.Element('root')
-#etree.SubElement(root, 'element').text = 'test text'
-#xmlString = etree.tostring(root, pretty_print=True)
-#f = open('test.xml', 'w')
-#f.write(xmlString.decode('utf-8'))
-#f.close()
