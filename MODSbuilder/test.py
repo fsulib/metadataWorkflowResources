ec1755c9f1471a13b11ccdace3e7453afac3c9c4
mrmiguez <r.m.miguez@gmail.com>
2015-06-19 06:55:24 -0400

    update

--- MODSbuilder/test.py
+++ MODSbuilder/test.py
@@ -4,18 +4,29 @@ import sys
 
 parents = {'languageTerm': 'language', 'physicalLocation': 'location', 'namePart': 'name', 'role': 'name', 'roleTerm': 'role', 'dateCreated': 'originInfo', 'dateIssued': 'originInfo', 'issuance': 'originInfo', 'place': 'originInfo', 'placeTerm': 'place', 'publisher': 'originInfo', 'digitalOrigin': 'physicalDescription', 'extent': 'physicalDescription', 'form': 'physicalDescription', 'descriptionStandard': 'recordInfo', 'recordCreationDate': 'recordInfo', 'recordOrigin': 'recordInfo', 'nonSort': 'titleInfo', 'title': 'titleInfo', 'subTitle': 'titleInfo', 'abstract': 'mods', 'accessCondition': 'mods', 'extension': 'mods', 'genre': 'mods', 'identifier': 'mods', 'language': 'mods', 'location': 'mods', 'name': 'mods', 'note': 'mods', 'originInfo': 'mods', 'physicalDescription': 'mods', 'recordInfo': 'mods', 'titleInfo': 'mods', 'typeOfResource': 'mods'}
 
-class elem(etree.Elementbase):
-
-  def Parent(self, parent):
-    self.parent = parent
+class elem(etree.ElementBase):
+  Parent = parents[tag]
     
+def makeMODS(tag, text):
+  el = elem(tag)
+  el.text = text
+  el.Parent = parents[tag]
+  if el.Parent is None:
+      host = makeMODS(parents[tag])
+      host.append(el)
+      return host
+  else:
+      return el
+
 def buildRecord(record):
-  root = etree.Element('mods', nsmap = NSmap)
+  root = etree.Element('mods')
   for tag, text in record.items():
-    el = etree.Element(tag)
-    el.text = text
-    el.Parent = parents[tag]
-    if el.Parent is None:
+    el = makeMODS(tag, text)
+    root.append(el)
+#    el = etree.Element(tag)
+#    el.text = text
+#    el.Parent = parents[tag]
+#    if el.Parent is None:
       
   xmlString = etree.tostring(root, pretty_print=True)
   f = open('output/%s.xml' % record['identifier'], 'w')
