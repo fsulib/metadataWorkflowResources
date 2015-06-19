a51a9a9967aa964e5a45f55e16ec99cd6f63b973
mrmiguez <r.m.miguez@gmail.com>
2015-06-19 05:18:25 -0400

    kinda sorta working

--- MODSbuilder/MODSbuilder-dev.py
+++ MODSbuilder/MODSbuilder-dev.py
@@ -3,23 +3,26 @@ import csv
 import sys
 import os
 
+parents = {'mods': '', 'languageTerm': 'language', 'physicalLocation': 'location', 'namePart': 'name', 'role': 'name', 'roleTerm': 'role', 'dateCreated': 'originInfo', 'dateIssued': 'originInfo', 'issuance': 'originInfo', 'place': 'originInfo', 'placeTerm': 'place', 'publisher': 'originInfo', 'digitalOrigin': 'physicalDescription', 'extent': 'physicalDescription', 'form': 'physicalDescription', 'descriptionStandard': 'recordInfo', 'recordCreationDate': 'recordInfo', 'recordOrigin': 'recordInfo', 'nonSort': 'titleInfo', 'title': 'titleInfo', 'subTitle': 'titleInfo', 'abstract': 'mods', 'accessCondition': 'mods', 'extension': 'mods', 'genre': 'mods', 'identifier': 'mods', 'language': 'mods', 'location': 'mods', 'name': 'mods', 'note': 'mods', 'originInfo': 'mods', 'physicalDescription': 'mods', 'recordInfo': 'mods', 'titleInfo': 'mods', 'typeOfResource': 'mods'}
 modsNS = 'http://www.loc.gov/mods/v3'
 flvcNS = 'info:flvc/manifest/v1'
 NSmap = {None: modsNS, 'flvc': flvcNS}
 
-def makeMODS(tag, text):
+def makeMODS(tag, text = None, mods = None):
   elem = etree.Element(tag)
-  elem.text = text
-  if parent[elem.tag] is None:
-    makeMODS(parent[elem.tag])
+  if text:
+    elem.text = text
+  if parents[elem.tag] is None:
+    host = makeMODS(parents[elem.tag])
+    host.append(elem)
   else:
-    root.append(elem)  
+    mods.append(elem)  
     
 def buildRecord(record):
-  root = etree.Element('mods', nsmap = NSmap)
+  mods = etree.Element('mods', nsmap = NSmap)
   for tag, text in record.items():
-    makeMODS(tag, text)
-  xmlString = etree.tostring(root, pretty_print=True)
+    makeMODS(tag, text, mods)
+  xmlString = etree.tostring(mods, pretty_print=True)
   f = open('output/%s.xml' % record['identifier'], 'w')
   f.write(xmlString.decode('utf-8'))
   f.close()
