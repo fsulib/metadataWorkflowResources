d53d6af81d2cb2579fa4e0127327a08afcddb5d7
mrmiguez <r.m.miguez@gmail.com>
2015-12-02 09:00:52 -0500

    updating

---
+++ personagraphyCSV.py
@@ -0,0 +1,19 @@
+import csv
+from lxml import etree
+import os
+import sys
+
+name = os.path.splitext(sys.argv[1])[0]
+personsCSV = open(name +'.csv', newline='')
+persons =  csv.DictReader(personsCSV)
+root = etree.Element('names')
+for line in persons:
+    person = etree.Element('name', authority='lcnaf', authorityURI='http://id.loc.gov/authorities/names', valueURI=line['valueURI'])
+    for key in line.keys():
+        if key != 'valueURI':
+            namePart = etree.Element('namePart', type=key)
+            namePart.text = line[key]
+            person.append(namePart)
+    root.append(person)
+tree = root.getroottree()
+tree.write(name +'.xml', pretty_print=True, xml_declaration=True)
