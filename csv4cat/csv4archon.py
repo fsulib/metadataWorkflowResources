e3228de4282a5d2748da332b2307388a4b669175
mrmiguez <r.m.miguez@gmail.com>
2016-01-18 08:57:20 -0500

    updating

--- csv4cat/csv4archon.py
+++ csv4cat/csv4archon.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from lxml import etee
 import csv
 import os
@@ -5,9 +7,17 @@ import sys
 
 NS = {'mods':'http://www.loc.gov/mods/v3'}
 
-def writeCSV(filename):
+def writeCSV(filename, arg):
+    if arg == 'archon':
+
+    elif arg == 'aleph':
+
+    else:
+        print('Unrecognized system argument.')
     
 
 name = os.path.splitext(sys.argv[1])[0]
-writeCSV(name)
+arg = sys.argv[2]
+os.sys('python2 ~/bin/pyoaiharvester/pyoaiharvester.py -l http://fsu.digital.flvc.org/oai2 -s {0} -o {1} -m mods'.format(name, name +'.xml'))
+writeCSV(name, arg)
 print('Spreadsheet created.')
