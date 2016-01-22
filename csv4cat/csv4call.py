85e44f366a6710e426ee25a049320881a533fff1
mrmiguez <r.m.miguez@gmail.com>
2016-01-22 13:50:32 -0500

    notes for archon CSV

--- csv4cat/csv4call.py
+++ csv4cat/csv4call.py
@@ -55,35 +55,30 @@ def archon(filename):
   header = ['collID;', 'series;' 'subseries;', 'box;', 'folder;', 'title;', 'date;', 'description;', 'PURL;']
   NS = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'dc': 'http://purl.org/dc/elements/1.1/', 'mods': 'http://www.loc.gov/mods/v3', 'dcterms': 'http://purl.org/dc/terms'}
   with open(fileName + '.csv', 'w') as f:
-    writer = csv.writer(f, delimiter=' ')
+    writer = csv.writer(f, delimiter=',')
     writer.writerow(header)
     tree = ET.parse(fileName + '.xml')
     root = tree.getroot()
+# fields to pull: collection ID, Series #, Subseries #, Box #, Folder #, Item #, Title, Date, Description (i.e. PURL)
+# first row should be new digital series
     for record in root.iterfind('.//{%s}mods' % NS['mods'] ):
       data = []
       for identifier in record.iterfind('.//{%s}identifier' % NS['mods']):
         m = purl.search(identifier.text)
         if m:
           data.append(m.group())
-          data.append(';')
       for identifier in record.iterfind('.//{%s}identifier' % NS['mods']):
         m = pid.search(identifier.text)
         if m:
           data.append(m.group())
-          data.append(';')
       for title in record.iterfind('.//{%s}titleInfo' % NS['mods']):
         data.append('%s' % title.text)
-      data.append(';')        
       for creator in record.iterfind('.//{%s}name' % NS['mods']):
         data.append('%s' % creator.text)
-      data.append(';')      
       for date in record.iterfind('.//{%s}date' % NS['mods']):
         data.append('%s' % date.text)
-      data.append(';')
       for description in record.iterfind('.//{%s}abstract' % NS['mods']):
         data.append('%s' % description.text)
-      data.append(';')
-      data.append(';')
       writer.writerow(data)
 
 #name = os.path.splitext(sys.argv[1])[0]
