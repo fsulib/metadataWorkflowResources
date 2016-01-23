b00e4becd068582e719600f4f0fbcfef7333da55
mrmiguez <r.m.miguez@gmail.com>
2016-01-23 10:21:25 -0500

    updates from testing

--- csv4cat/csv4call.py
+++ csv4cat/csv4call.py
@@ -1,55 +1,56 @@
-import xml.etree.ElementTree as ET
+from lxml import etree
 import csv
 import sys
 import os
 import re
 
 def aleph(fileName):
-  purl = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
-  pid = re.compile('fsu:[0-9]*')
-  header = ['PURL', 'PID' 'Title', 'Creator', 'Date', 'Notes', 'Comments/Shares']
-  NS = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'dc': 'http://purl.org/dc/elements/1.1/', 'mods': 'http://www.loc.gov/mods/v3', 'dcterms': 'http://purl.org/dc/terms'}
-  with open(fileName + '.csv', 'w') as f:
-    writer = csv.writer(f, delimiter=',')
-    writer.writerow(header)
-    tree = ET.parse(fileName + '.xml')
-    root = tree.getroot()
-    for record in root.iterfind('.//{%s}mods' % NS['mods'] ):
-      data = []
-      for url in record.iterfind('./{%s}location/{%s}url' % NS['mods']):
-        m = purl.search(identifier.text)
-        if m:
-          data.append(m.group())
-        else:
-          data.append('null')
-#          data.append(';')
-      for identifier in record.iterfind('.//{%s}identifier' % NS['mods']):
-        m = pid.search(identifier.text)
-        if m:
-          data.append(m.group())
+  print('aleph')
+#  purl = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
+#  pid = re.compile('fsu:[0-9]*')
+#  header = ['PURL', 'PID' 'Title', 'Creator', 'Date', 'Notes', 'Comments/Shares']
+#  NS = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'dc': 'http://purl.org/dc/elements/1.1/', 'mods': 'http://www.loc.gov/mods/v3', 'dcterms': 'http://purl.org/dc/terms'}
+#  with open(fileName + '.csv', 'w') as f:
+#    writer = csv.writer(f, delimiter=',')
+#    writer.writerow(header)
+#    tree = etree.parse(fileName + '.xml')
+#    root = tree.getroot()
+#    for record in root.iterfind('.//{%s}mods' % NS['mods'] ):
+#      data = []
+#      for url in record.iterfind('./{%s}location/{%s}url' % NS['mods']):
+#        m = purl.search(identifier.text)
+#        if m:
+#          data.append(m.group())
 #        else:
 #          data.append('null')
 #          data.append(';')
-      for title in record.iterfind('.//{%s}titleInfo' % NS['mods']):
-        if title.find('./{%s}nonSort' % NS['mods']):
-          title-full = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text
-          if title.find('./{%s}subTitle' % NS['mods']):
-            title-full = title-full + ': ' + title.find('./{%s}subTitle' % NS['mods']).text
-        data.append('%s' % title-full)
+#      for identifier in record.iterfind('.//{%s}identifier' % NS['mods']):
+#        m = pid.search(identifier.text)
+#        if m:
+#          data.append(m.group())
+#        else:
+#          data.append('null')
+#          data.append(';')
+#      for title in record.iterfind('.//{%s}titleInfo' % NS['mods']):
+#        if title.find('./{%s}nonSort' % NS['mods']):
+#          title-full = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text
+#          if title.find('./{%s}subTitle' % NS['mods']):
+#            title-full = title-full + ': ' + title.find('./{%s}subTitle' % NS['mods']).text
+#        data.append('%s' % title-full)
 #       data.append(';')        
-      for creator in record.iterfind('.//{%s}name' % NS['mods']):
-        data.append('%s' % creator.text)
+#      for creator in record.iterfind('.//{%s}name' % NS['mods']):
+#        data.append('%s' % creator.text)
 #      data.append(';')      
-      for date in record.iterfind('.//{%s}date' % NS['mods']):
-        data.append('%s' % date.text)
+#      for date in record.iterfind('.//{%s}date' % NS['mods']):
+#        data.append('%s' % date.text)
 #      data.append(';')
-      for description in record.iterfind('.//{%s}abstract' % NS['mods']):
-        data.append('%s' % description.text)
+#      for description in record.iterfind('.//{%s}abstract' % NS['mods']):
+#        data.append('%s' % description.text)
 #      data.append(';')
 #      data.append(';')
-      writer.writerow(data)
+#      writer.writerow(data)
 
-def archon(filename):
+def archon(fileName):
   series = input("Enter a number for the digital series: ")
   collNum = input("Enter a collection ID number: ")
   purl = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
@@ -58,31 +59,44 @@ def archon(filename):
     writer = csv.writer(f, delimiter=',')
     data = [collNum, str(series), "", "", "", "", 'Items available online', "", "Copy the link into your browser. Items hosted in Digital Collections at DigiNole."]
     writer.writerow(data)
-    tree = ET.parse(fileName + '.xml')
+    tree = etree.parse(fileName + '.xml')
     root = tree.getroot()
-# fields to pull: collection ID, Series #, Subseries #, Box #, Folder #, Item #, Title, Date, Description (i.e. PURL)
-# first row should be new digital series
+    n = 1
+
+    # fields to pull: collection ID, Series #, Subseries #, Box #, Folder #, Item #, Title, Date, Description (i.e. PURL)
     for record in root.iterfind('.//{%s}mods' % NS['mods'] ):
-      n = 1
       data = [collNum, str(series), "", "", "", str(n)]
-      for identifier in record.iterfind('.//{%s}identifier' % NS['mods']):
-        m = purl.search(identifier.text)
-        if m:
-          data.append(m.group())
-      for title in record.iterfind('.//{%s}title' % NS['mods']):
-        m = pid.search(identifier.text)
-        if m:
-          data.append(m.group())
+
+      #title
       for title in record.iterfind('.//{%s}titleInfo' % NS['mods']):
-        data.append('%s' % title.text)
-      for creator in record.iterfind('.//{%s}name' % NS['mods']):
-        data.append('%s' % creator.text)
-      for date in record.iterfind('.//{%s}date' % NS['mods']):
-        data.append('%s' % date.text)
-      for description in record.iterfind('.//{%s}abstract' % NS['mods']):
-        data.append('%s' % description.text)
+        if title.find('./{%s}nonSort' % NS['mods']) is not None and title.find('./{%s}title' % NS['mods']) is not None and title.find('./{%s}subTitle' % NS['mods']) is not None:
+          titleFull = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text + ': ' + title.find('./{%s}subTitle' % NS['mods']).text
+        elif title.find('./{%s}nonSort' % NS['mods']) is not None and title.find('./{%s}title' % NS['mods']) is not None:
+          titleFull = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text
+        else:
+          titleFull = title.find('./{%s}title' % NS['mods']).text
+        data.append(titleFull)
+
+      #date
+      if record.find('./{%s}originInfo/{%s}copyrightDate' % (NS['mods'], NS['mods'])) is not None:
+        date = record.find('./{%s}originInfo/{%s}copyrightDate' % (NS['mods'], NS['mods'])).text
+      elif record.find('./{%s}originInfo/{%s}dateCreated' % (NS['mods'], NS['mods'])) is not None:
+        date = record.find('./{%s}originInfo/{%s}dateCreated' % (NS['mods'], NS['mods'])).text
+      elif record.find('./{%s}originInfo/{%s}dateIssued' % (NS['mods'], NS['mods'])) is not None:
+        date = record.find('./{%s}originInfo/{%s}dateIssued' % (NS['mods'], NS['mods'])).text
+      elif record.find('./{%s}originInfo/{%s}dateOther' % (NS['mods'], NS['mods'])) is not None:
+        date = record.find('./{%s}originInfo/{%s}dateOther' % (NS['mods'], NS['mods'])).text
+      else:
+        date = ""
+      data.append(date)
+      
+      #description (PURL)
+      
+
+#      for description in record.iterfind('.//{%s}abstract' % NS['mods']):
+#        data.append('%s' % description.text)
       writer.writerow(data)
-      n++
+      n = n + 1
 
 #name = os.path.splitext(sys.argv[1])[0]
 #writeCSV(name)
--- csv4cat/dev/test.py
+++ csv4cat/dev/test.py
@@ -7,20 +7,29 @@ import re
 NS = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'dc': 'http://purl.org/dc/elements/1.1/', 'mods': 'http://www.loc.gov/mods/v3', 'dcterms': 'http://purl.org/dc/terms'}
 tree = etree.parse('fsu_nap01.xml')
 root = tree.getroot()
+purl = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
 for record in root.iterfind('.//{%s}mods' % NS['mods']):
+  print('record')
+  for url in record.find('./{%s}location/{%s}url' % (NS['mods'], NS['mods'])):
+    print('url')
+    m = purl.search(url.text)
+    if m:
+      print(m.group())
+
+
 #  if 'keyDate' in record.xpath('./{%s}originInfo/{%s}*' % (NS['mods'], NS['mods'])).attrib:
 #    date = record.xpath('./{%s}originInfo/{%s}*' % (NS['mods'], NS['mods'])).text
-  if record.find('./{%s}originInfo/{%s}copyrightDate' % (NS['mods'], NS['mods'])) is not None:
-    date = record.find('./{%s}originInfo/{%s}copyrightDate' % (NS['mods'], NS['mods'])).text
-  elif record.find('./{%s}originInfo/{%s}dateCreated' % (NS['mods'], NS['mods'])) is not None:
-    date = record.find('./{%s}originInfo/{%s}dateCreated' % (NS['mods'], NS['mods'])).text
-  elif record.find('./{%s}originInfo/{%s}dateIssued' % (NS['mods'], NS['mods'])) is not None:
-    date = record.find('./{%s}originInfo/{%s}dateIssued' % (NS['mods'], NS['mods'])).text
-  elif record.find('./{%s}originInfo/{%s}dateOther' % (NS['mods'], NS['mods'])) is not None:
-    date = record.find('./{%s}originInfo/{%s}dateOther' % (NS['mods'], NS['mods'])).text
-  else:
-    date = 'No'
-  print(date)
+#  if record.find('./{%s}originInfo/{%s}copyrightDate' % (NS['mods'], NS['mods'])) is not None:
+#    date = record.find('./{%s}originInfo/{%s}copyrightDate' % (NS['mods'], NS['mods'])).text
+#  elif record.find('./{%s}originInfo/{%s}dateCreated' % (NS['mods'], NS['mods'])) is not None:
+#    date = record.find('./{%s}originInfo/{%s}dateCreated' % (NS['mods'], NS['mods'])).text
+#  elif record.find('./{%s}originInfo/{%s}dateIssued' % (NS['mods'], NS['mods'])) is not None:
+#    date = record.find('./{%s}originInfo/{%s}dateIssued' % (NS['mods'], NS['mods'])).text
+#  elif record.find('./{%s}originInfo/{%s}dateOther' % (NS['mods'], NS['mods'])) is not None:
+#    date = record.find('./{%s}originInfo/{%s}dateOther' % (NS['mods'], NS['mods'])).text
+#  else:
+#    date = 'No'
+#  print(date)
 
 
 #  for title in record.iterfind('.//{%s}titleInfo' % NS['mods']):
