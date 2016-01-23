1ffd4a873f6baff3a2cf0c0ca9fc034d12861924
mrmiguez <r.m.miguez@gmail.com>
2016-01-23 09:40:59 -0500

    archon dev

--- csv4cat/dev/test.py
+++ csv4cat/dev/test.py
@@ -8,14 +8,29 @@ NS = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'dc': 'http://pur
 tree = etree.parse('fsu_nap01.xml')
 root = tree.getroot()
 for record in root.iterfind('.//{%s}mods' % NS['mods']):
-  for title in record.iterfind('.//{%s}titleInfo' % NS['mods']):
-    if title.elem('./{%s}nonSort' % NS['mods']) is not None and title.elem('./{%s}title' % NS['mods']) is not None and title.elem('./{%s}subTitle' % NS['mods']) is not None:
-      titleFull = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text + ': ' + title.find('./{%s}subTitle' % NS['mods']).text
-    elif title.elem('./{%s}nonSort' % NS['mods']) is not None and title.elem('./{%s}title' % NS['mods']) is not None:
-      titleFull = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text
-    else:
-      titleFull = title.find('./{%s}title' % NS['mods']).text
-    print(titleFull)
+#  if 'keyDate' in record.xpath('./{%s}originInfo/{%s}*' % (NS['mods'], NS['mods'])).attrib:
+#    date = record.xpath('./{%s}originInfo/{%s}*' % (NS['mods'], NS['mods'])).text
+  if record.find('./{%s}originInfo/{%s}copyrightDate' % (NS['mods'], NS['mods'])) is not None:
+    date = record.find('./{%s}originInfo/{%s}copyrightDate' % (NS['mods'], NS['mods'])).text
+  elif record.find('./{%s}originInfo/{%s}dateCreated' % (NS['mods'], NS['mods'])) is not None:
+    date = record.find('./{%s}originInfo/{%s}dateCreated' % (NS['mods'], NS['mods'])).text
+  elif record.find('./{%s}originInfo/{%s}dateIssued' % (NS['mods'], NS['mods'])) is not None:
+    date = record.find('./{%s}originInfo/{%s}dateIssued' % (NS['mods'], NS['mods'])).text
+  elif record.find('./{%s}originInfo/{%s}dateOther' % (NS['mods'], NS['mods'])) is not None:
+    date = record.find('./{%s}originInfo/{%s}dateOther' % (NS['mods'], NS['mods'])).text
+  else:
+    date = 'No'
+  print(date)
+
+
+#  for title in record.iterfind('.//{%s}titleInfo' % NS['mods']):
+#    if title.find('./{%s}nonSort' % NS['mods']) is not None and title.find('./{%s}title' % NS['mods']) is not None and title.find('./{%s}subTitle' % NS['mods']) is not None:
+#      titleFull = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text + ': ' + title.find('./{%s}subTitle' % NS['mods']).text
+#    elif title.find('./{%s}nonSort' % NS['mods']) is not None and title.find('./{%s}title' % NS['mods']) is not None:
+#      titleFull = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text
+#    else:
+#      titleFull = title.find('./{%s}title' % NS['mods']).text
+#    print(titleFull)
 # future-proofing warning for below 
 #    if title.find('./{%s}title' % NS['mods']) and title.find('./{%s}nonSort' % NS['mods']) and title.find('./{%s}subTitle' % NS['mods']):
 #    print(title.find('./{%s}title' % NS['mods']).text)
