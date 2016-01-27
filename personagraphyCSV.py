ef232dd8c0d9dbdfeba00bbedc971abea5c81b73
mrmiguez <r.m.miguez@gmail.com>
2016-01-27 11:12:08 -0500

    refining authorities process

--- personagraphyCSV.py
+++ personagraphyCSV.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import csv
 from lxml import etree
 import os
@@ -8,12 +10,12 @@ personsCSV = open(name +'.csv', newline='')
 persons =  csv.DictReader(personsCSV)
 root = etree.Element('names')
 for line in persons:
-    person = etree.Element('name', type='personal', authority='lcnaf', authorityURI='http://id.loc.gov/authorities/names', valueURI=line['valueURI'])
-    for key in line.keys():
-        if key != 'valueURI':
-            namePart = etree.Element('namePart', type=key)
-            namePart.text = line[key]
-            person.append(namePart)
-    root.append(person)
+	person = etree.Element('name', type='personal', authority='lcnaf', authorityURI='http://id.loc.gov/authorities/names', valueURI=line['valueURI'])
+	for key in line.keys():
+		if key != 'valueURI':
+			namePart = etree.Element('namePart', type=key)
+			namePart.text = line[key]
+			person.append(namePart)
+	root.append(person)
 tree = root.getroottree()
 tree.write(name +'.xml', pretty_print=True, xml_declaration=True)
