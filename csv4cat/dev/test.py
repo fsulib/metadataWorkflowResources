7b25dfca0385140937fca7fd48713cdb06db3c1d
mrmiguez <r.m.miguez@gmail.com>
2016-01-28 12:03:16 -0500

    fighting names

--- csv4cat/dev/test.py
+++ csv4cat/dev/test.py
@@ -4,21 +4,64 @@ import sys
 import os
 import re
 
-'''
-def nameGenerator(name, NS, fullName):
-  names = {}
-  for namePart in name.iterfind('./{%s}namePart' % NS['mods']):
-    if len(namePart.attrib) == 0:
-#      print(namePart.text)
-      fullName = fullName + namePart.text
+def nameGen(names, fullName):
+#  print('nameGen', names)
+  keys = []
+  for key in names.keys():
+    keys.append(key)
+#  print(keys)  
+  if 'family' 'given' 'termsOfAddress' 'date' in keys:
+    fullName = fullName + names['family'] + ', ' + names['given'] + ', ' + names['termsOfAddress'] + ' ' + names['date']
+  elif 'family' 'given' and 'date' in keys:
+    fullName = fullName + names['family'] + ', ' + names['given'] + ' ' + names['date']
+  elif 'family' 'given' 'termsOfAddress' in keys:
+    fullName = fullName + names['family'] + ', ' + names['given'] + ', ' + names['termsOfAddress']
+  elif 'family' 'termsOfAddress' 'date' in keys:
+    fullName = fullName + names['family'] + ', ' + names['termsOfAddress'] + ' ' + names['date']
+  elif 'given' 'termsOfAddress' 'date' in keys:
+    fullName = fullName + names['given'] + ', ' + names['termsOfAddress'] + ' ' + names['date']
+  elif 'family' 'given' in keys:
+    fullName = fullName + names['family'] + ', ' + names['given']
+  elif 'family' 'date' in keys:
+    fullName = fullName + names['family'] + ', ' + names['date']
+  elif 'family' 'termsOfAddress' in keys:
+    fullName = fullName + names['family'] + ', ' + names['termsOfAddress']
+  elif 'given' 'date' in keys:
+    fullName = fullName + names['given'] + ', ' + names['date']
+  elif 'given' 'termsOfAddress' in keys:
+    fullName = fullName + names['given'] + ', ' + names['termsOfAddress']
+  elif 'termsOfAddress' 'date' in keys:
+    fullName = fullName + ', ' + names['termsOfAddress'] + ' ' + names['date']
+  elif 'date' in keys:
+    fullName = fullName + ', ' + names['date']
+  elif 'termsOfAddress' in keys:
+    fullName = fullName + ', ' + names['termsOfAddress']
+  return fullName
+  
+NS = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'dc': 'http://purl.org/dc/elements/1.1/', 'mods': 'http://www.loc.gov/mods/v3', 'dcterms': 'http://purl.org/dc/terms'}
+tree = etree.parse('fsu_cookbooksandherbals.xml')
+root = tree.getroot()
+purl = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
+for record in root.iterfind('.//{%s}mods' % NS['mods']):
+  allNames = ""
+  for name in record.iterfind('./{%s}name' % NS['mods']):
+    fullName = ""
+    if len(name.findall('./{%s}namePart' % NS['mods'])) > 1:
+      #print('Multipart name')
+      names = {}
+      for namePart in name.findall('./{%s}namePart' % NS['mods']):
+        if 'type' not in namePart.keys():
+          fullName = namePart.text
+        elif 'type' in namePart.keys():
+          names[namePart.attrib['type']] = namePart.text
+      print(names)
+      nameGen(names, fullName)
     else:
-      for key, value in namePart.attrib.items():
-#        print(value, namePart.text)
-        names[value] = namePart.text
-        print(fullName, namePart.text, value)
-'''
-
-
+      #print('Single part name')
+      fullName = fullName + name.find('./{%s}namePart' % NS['mods']).text
+    allNames = allNames + fullName + ' || '
+  print(allNames)
+			
 '''
   if 'family' and 'given' and 'termsOfAddress' and 'date' in names.keys():
     fullName = fullName + names['family'] + ', ' + names['given'] + ', ' + names['termsOfAddress'] + ' ' + names['date']
@@ -36,39 +79,9 @@ def nameGenerator(name, NS, fullName):
     fullName = fullName + names['family'] + ', ' + names['termsOfAddress']
   return fullName
 '''
-NS = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'dc': 'http://purl.org/dc/elements/1.1/', 'mods': 'http://www.loc.gov/mods/v3', 'dcterms': 'http://purl.org/dc/terms'}
-tree = etree.parse('fsu_cookbooksandherbals.xml')
-root = tree.getroot()
-purl = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
-for record in root.iterfind('.//{%s}mods' % NS['mods']):
-  allNames = ""
-  for name in record.iterfind('./{%s}name' % NS['mods']):
-    fullName = ""
-    if len(name.findall('./{%s}namePart' % NS['mods'])) > 1:
-      print('trace01')
-      if name.xpath('./child::namePart[attribute::type="family"]'):
-        print('family name')
-#        fullName = fullName + name.find('./{%s}namePart[type="family"]' % NS['mods']).text
-        if name.find('./{%s}namePart[type="given"]' % NS['mods']):
-          fullName = fullName + ', ' + name.find('./{%s}namePart[type="given"]' % NS['mods']).text
-      else:
-        print('trace03')
-        if len(name.find('./{%s}namePart' % NS['mods']).attrib.items()) == 0:
-          fullName = fullName + name.find('./{%s}namePart' % NS['mods']).text
-      if name.xpath('./child::namePart[attribute::type="termsOfAddress"]'):
-        fullName = fullName + ', ' + name.xpath('./child::namePart[attribute::type="termsOfAddress"]').text
-      if name.xpath('./child::namePart[attribute::type="date"]'):
-        fullName = fullName + ', ' + name.xpath('./child::namePart[attribute::type="date"]').text
-    else:
-      print('trace02')
-      fullName = fullName + name.find('./{%s}namePart' % NS['mods']).text
-    allNames = allNames + fullName + ' || '
-    print(allNames)
     
     
-    
-    
-    '''
+'''
     #XPath example: child::para[attribute::type="warning"]
       if 'type' in name.find('./{%s}namePart' % NS['mods']).attrib:
         print('Parted name...')        
@@ -93,55 +106,9 @@ for record in root.iterfind('.//{%s}mods' % NS['mods']):
         fullName = fullName + name.find('./{%s}namePart[type="given"]' % NS['mods']).text
       if name.find('./{%s}namePart[type="date"]' % NS['mods']) is not None:
         fullName = fullName + ' ' + name.find('./{%s}namePart[type="date"]' % NS['mods']).text
- '''   
+'''   
 #    if name.attrib['type'] == 'corporate':
 #      names.append('corporate')
 #  for item in names:
 #    items = items + item
 #  print(items)
-    
-
-
-
-#  for url in record.iterfind('./{%s}location/{%s}url' % (NS['mods'], NS['mods'])):
-#    m = purl.search(url.text)
-#    if m:
-#      print(m.group())
-
-
-#  if 'keyDate' in record.xpath('./{%s}originInfo/{%s}*' % (NS['mods'], NS['mods'])).attrib:
-#    date = record.xpath('./{%s}originInfo/{%s}*' % (NS['mods'], NS['mods'])).text
-#  if record.find('./{%s}originInfo/{%s}copyrightDate' % (NS['mods'], NS['mods'])) is not None:
-#    date = record.find('./{%s}originInfo/{%s}copyrightDate' % (NS['mods'], NS['mods'])).text
-#  elif record.find('./{%s}originInfo/{%s}dateCreated' % (NS['mods'], NS['mods'])) is not None:
-#    date = record.find('./{%s}originInfo/{%s}dateCreated' % (NS['mods'], NS['mods'])).text
-#  elif record.find('./{%s}originInfo/{%s}dateIssued' % (NS['mods'], NS['mods'])) is not None:
-#    date = record.find('./{%s}originInfo/{%s}dateIssued' % (NS['mods'], NS['mods'])).text
-#  elif record.find('./{%s}originInfo/{%s}dateOther' % (NS['mods'], NS['mods'])) is not None:
-#    date = record.find('./{%s}originInfo/{%s}dateOther' % (NS['mods'], NS['mods'])).text
-#  else:
-#    date = 'No'
-#  print(date)
-
-
-#  for title in record.iterfind('.//{%s}titleInfo' % NS['mods']):
-#    if title.find('./{%s}nonSort' % NS['mods']) is not None and title.find('./{%s}title' % NS['mods']) is not None and title.find('./{%s}subTitle' % NS['mods']) is not None:
-#      titleFull = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text + ': ' + title.find('./{%s}subTitle' % NS['mods']).text
-#    elif title.find('./{%s}nonSort' % NS['mods']) is not None and title.find('./{%s}title' % NS['mods']) is not None:
-#      titleFull = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text
-#    else:
-#      titleFull = title.find('./{%s}title' % NS['mods']).text
-#    print(titleFull)
-# future-proofing warning for below 
-#    if title.find('./{%s}title' % NS['mods']) and title.find('./{%s}nonSort' % NS['mods']) and title.find('./{%s}subTitle' % NS['mods']):
-#    print(title.find('./{%s}title' % NS['mods']).text)
-#      titleFull = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text + ': ' + title.find('./{%s}subTitle' % NS['mods']).text
-#    elif title.find('./{%s}title' % NS['mods']) and title.find('./{%s}nonSort' % NS['mods']):
-#      titleFull = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text
-#    else:
-#      titleFull = title.find('./{%s}title' % NS['mods']).text
-#  print(titleFull)
-#    if title.find('./{%s}nonSort' % NS['mods']):
-#      title-full = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text
-#        if title.find('./{%s}subTitle' % NS['mods']):
-#          title-full = title-full + ': ' + title.find('./{%s}subTitle' % NS['mods']).text
