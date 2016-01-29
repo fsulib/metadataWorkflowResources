af75b8424b756e7294956884114533e56e76da05
mrmiguez <r.m.miguez@gmail.com>
2016-01-29 13:36:09 -0500

    names integrated

--- csv4cat/csv4call.py
+++ csv4cat/csv4call.py
@@ -2,6 +2,38 @@ from lxml import etree
 import csv
 import re
 
+def nameGen(names, fullName):
+  keys = []
+  for key in names.keys():
+    keys.append(key)
+  if all(x in keys for x in ['family', 'given', 'termsOfAddress', 'date']):
+    fullName = fullName + names['family'] + ', ' + names['given'] + ', ' + names['termsOfAddress'] + ' ' + names['date']
+  elif all(x in keys for x in ['family', 'given', 'date']):
+    fullName = fullName + names['family'] + ', ' + names['given'] + ' ' + names['date']
+  elif all(x in keys for x in ['family', 'given', 'termsOfAddress']):
+    fullName = fullName + names['family'] + ', ' + names['given'] + ', ' + names['termsOfAddress']
+  elif all(x in keys for x in ['family', 'termsOfAddress', 'date']):
+    fullName = fullName + names['family'] + ', ' + names['termsOfAddress'] + ' ' + names['date']
+  elif all(x in keys for x in ['given', 'termsOfAddress', 'date']):
+    fullName = fullName + names['given'] + ', ' + names['termsOfAddress'] + ' ' + names['date']
+  elif all(x in keys for x in ['family', 'given']):
+    fullName = fullName + names['family'] + ', ' + names['given']
+  elif all(x in keys for x in ['family', 'date']):
+    fullName = fullName + names['family'] + ', ' + names['date']
+  elif all(x in keys for x in ['family', 'termsOfAddress']):
+    fullName = fullName + names['family'] + ', ' + names['termsOfAddress']
+  elif all(x in keys for x in ['given', 'date']):
+    fullName = fullName + names['given'] + ', ' + names['date']
+  elif all(x in keys for x in ['given', 'termsOfAddress']):
+    fullName = fullName + names['given'] + ', ' + names['termsOfAddress']
+  elif all(x in keys for x in ['termsOfAddress', 'date']):
+    fullName = fullName + ', ' + names['termsOfAddress'] + ' ' + names['date']
+  elif 'date' in keys:
+    fullName = fullName + ', ' + names['date']
+  elif 'termsOfAddress' in keys:
+    fullName = fullName + ', ' + names['termsOfAddress']
+  return fullName
+
 def aleph(fileName):
   purl = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
   pid = re.compile('fsu:[0-9]*')
@@ -28,6 +60,7 @@ def aleph(fileName):
           data.append(m.group())
       
       #title
+      allTitles = []
       for title in record.iterfind('.//{%s}titleInfo' % NS['mods']):
         if title.find('./{%s}nonSort' % NS['mods']) is not None and title.find('./{%s}title' % NS['mods']) is not None and title.find('./{%s}subTitle' % NS['mods']) is not None:
           titleFull = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text + ': ' + title.find('./{%s}subTitle' % NS['mods']).text
@@ -35,26 +68,26 @@ def aleph(fileName):
           titleFull = title.find('./{%s}nonSort' % NS['mods']).text + ' ' + title.find('./{%s}title' % NS['mods']).text
         else:
           titleFull = title.find('./{%s}title' % NS['mods']).text
-        data.append(titleFull)
+        allTitles.append(titleFull)	 
+      data.append(allTitles)
       
       #names
-      allNames = ""
-      for name in record.iterfind('.//{%s}name' % NS['mods']):
+      allNames = []
+      for name in record.iterfind('./{%s}name' % NS['mods']):
         fullName = ""
         if len(name.findall('./{%s}namePart' % NS['mods'])) > 1:
+          #Multipart name
           names = {}
-          for namePart in name.iterfind('./{%s}namePart' % NS['mods']):
-            for key, value in namePart.attrib.items():
-              names[value] = namePart.text
-          if 'family' and 'given' and 'date' in names.keys():
-            fullName = names['family'] + ', ' + names['given'] + ' ' + names['date']
-          elif 'family' and 'given' and not 'date' in names.keys():
-            fullname = names['family'] + ', ' + names['given']
-          elif not 'family' and 'given' and 'date' in names.keys():
-            fullname = names['given'] + ' ' + names['date']    
+          for namePart in name.findall('./{%s}namePart' % NS['mods']):
+            if 'type' not in namePart.keys():
+              fullName = namePart.text
+            elif 'type' in namePart.keys():
+              names[namePart.attrib['type']] = namePart.text
+          fullName = nameGen(names, fullName)
         else:
-          fullName = name.find('./{%s}namePart' % NS['mods']).text
-        allNames = allNames + fullName + ' || '
+          #Single part name
+          fullName = fullName + name.find('./{%s}namePart' % NS['mods']).text
+        allNames.append(fullName)
       data.append(allNames)
       
       #date
