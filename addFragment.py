e3bad1efee5fd7fce26d5a21f8a741d244d937f4
mrmiguez <r.m.miguez@gmail.com>
2016-02-23 09:28:43 -0500

    additions to MODS by top-level fragments

--- addFragment.py
+++ addFragment.py
@@ -4,16 +4,16 @@ import sys
 import os
 from lxml import etree
 
-def addNames(mods, personography):
+def addFragment(mods, fragment):
     NS = {'mods':'http://www.loc.gov/mods/v3'}
     modsTree = etree.parse(mods)
     modsRoot = modsTree.getroot()
-    personTree = etree.parse(personography)
-    personRoot = personTree.getroot()
-    for name in personRoot.iter('name'):
+    fragmentTree = etree.parse(fragment)
+    fragmentRoot = fragmentTree.getroot()
+    for node in fragmentRoot.xpath('self::*'):
         subject = etree.Element('{%s}subject' %NS['mods'])
-        subject.append(name)
-        modsRoot.append(subject)
+        #subject.append(node)
+        modsRoot.append(node)
     modsTree.write(mods +'out', pretty_print=True, xml_declaration=True)
 
-addNames(sys.argv[1], sys.argv[2])
+addFragment(sys.argv[1], sys.argv[2])
