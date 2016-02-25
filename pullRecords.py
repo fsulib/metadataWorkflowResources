534c52a80d6379c365cf4a3699e4316abc5a3b9b
mrmiguez <r.m.miguez@gmail.com>
2016-02-25 15:49:23 -0500

    pull down records

---
+++ pullRecords.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import requests
+import sys
+
+def getRecords(pidFile):
+    for line in open(pidFile, 'r'):
+        r = requests.get('https://fsu.digital.flvc.org/islandora/object/{0}/datastream/MODS/view'.format(line.split('\n')[0]))
+        r.encoding = 'utf-8'
+        outFile = open('MODS/{0}.xml'.format(line.split('\n')[0]), 'w', encoding='UTF-8')
+        outFile.write(r.text)
+        outFile.close()
+
+getRecords(sys.argv[1])
