d19b71d4814e4dafbc96426a23b7d768e7b6dbe3
mrmiguez <r.m.miguez@gmail.com>
2016-03-01 11:09:30 -0500

    record pull

--- pullRecords.py
+++ pullRecords.py
@@ -5,7 +5,7 @@ import sys
 
 def getRecords(pidFile):
     for line in open(pidFile, 'r'):
-        r = requests.get('https://fsu.digital.flvc.org/islandora/object/{0}/datastream/MODS/view'.format(line.split('\n')[0]))
+        r = requests.get('https://fsu.digital.flvc.org/islandora/object/{0}/datastream/MODS/download'.format(line.split('\n')[0]))
         r.encoding = 'utf-8'
         outFile = open('MODS/{0}.xml'.format(line.split('\n')[0]), 'w', encoding='UTF-8')
         outFile.write(r.text)
