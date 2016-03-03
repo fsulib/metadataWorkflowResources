78cb1a978102be2156ed967dad25ad149535def1
mrmiguez <r.m.miguez@gmail.com>
2016-03-03 13:27:41 -0500

    fixed encoding errors

--- pullRecords.py
+++ pullRecords.py
@@ -6,8 +6,8 @@ import sys
 def getRecords(pidFile):
     for line in open(pidFile, 'r'):
         r = requests.get('https://fsu.digital.flvc.org/islandora/object/{0}/datastream/MODS/download'.format(line.split('\n')[0]))
-        r.encoding = 'utf-8'
-        outFile = open('MODS/{0}.xml'.format(line.split('\n')[0]), 'w', encoding='UTF-8')
+        r.encoding = 'utf8'
+        outFile = open('MODS/{0}.xml'.format(line.split('\n')[0]), 'w')
         outFile.write(r.text)
         outFile.close()
 
