8d89af65d50683704a58ea986cd16c908f9091e2
mrmiguez <r.m.miguez@gmail.com>
2016-03-08 16:05:14 -0500

    pulled records save with underscores replacing colons in file name

--- pullRecords.py
+++ pullRecords.py
@@ -7,7 +7,7 @@ def getRecords(pidFile):
     for line in open(pidFile, 'r'):
         r = requests.get('https://fsu.digital.flvc.org/islandora/object/{0}/datastream/MODS/download'.format(line.split('\n')[0]))
         r.encoding = 'utf8'
-        outFile = open('MODS/{0}.xml'.format(line.split('\n')[0]), 'w')
+        outFile = open('MODS/{0}.xml'.format(line.split('\n')[0]).replace(':', '_'), 'w')
         outFile.write(r.text)
         outFile.close()
 
