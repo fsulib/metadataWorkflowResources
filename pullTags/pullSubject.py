dd6e6809d76024da31fae4cbea3ad9a7da53d391
mrmiguez <r.m.miguez@gmail.com>
2016-03-15 10:58:47 -0400

    learning argparse

--- pullTags/pullSubject.py
+++ pullTags/pullSubject.py
@@ -1,16 +1,36 @@
+#!/usr/bin/env python3
+
 import xml.etree.ElementTree as ET
 import sys
 import os.path
+import argparse
 
 NS = {'mods' : 'http://www.loc.gov/mods/v3'}
 
-def pull(filename):
+def pullMODS(filename):
     tree = ET.parse(filename +'.xml')
     root = tree.getroot()
     for record in root.iterfind('.//{%s}mods' % NS['mods']):
         for subject in record.iterfind('.//{%s}subject' % NS['mods']):
             for child in subject:
                 print(child.text)
+                
+def pullOAI(filename):
+    print("oai_dc selected")
+    
+def pullDC(filename):
+    print("dc selected")    
 
-fileName = os.path.splitext(sys.argv[1])[0]
-pull(fileName)
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="input file")
+parser.add_argument("-m", "--metadataPrefix", help="select metadata encoding of source file (oai_dc, dc, or mods)")
+args = parser.parse_args()
+fileName = args.file.split(".")[0]
+if args.metadataPrefix == "mods":
+    pullMODS(fileName)
+elif args.metadataPrefix == "oai_dc":
+    pullOAI(fileName)
+elif args.metadataPrefix == "dc":
+    pullDC(fileName)
+else:
+    print("Invalid metadata prefix selected.")
