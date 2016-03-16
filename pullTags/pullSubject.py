983f4f409102b34ab86afc9caa0aa4d3cd71df4e
mrmiguez <r.m.miguez@gmail.com>
2016-03-16 08:26:33 -0400

    argparse :++:

--- pullTags/pullSubject.py
+++ pullTags/pullSubject.py
@@ -5,10 +5,11 @@ import sys
 import os.path
 import argparse
 
-NS = {'mods' : 'http://www.loc.gov/mods/v3'}
+NS = {'mods' : 'http://www.loc.gov/mods/v3', 'oai_dc' : 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+        'dc' : 'http://purl.org/dc/elements/1.1/'}
 
 def pullMODS(filename):
-    tree = ET.parse(filename +'.xml')
+    tree = ET.parse(filename)
     root = tree.getroot()
     for record in root.iterfind('.//{%s}mods' % NS['mods']):
         for subject in record.iterfind('.//{%s}subject' % NS['mods']):
@@ -16,16 +17,21 @@ def pullMODS(filename):
                 print(child.text)
                 
 def pullOAI(filename):
-    print("oai_dc selected")
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    for record in root.iterfind('.//{%s}dc' % NS['oai_dc']):
+        for subject in record.iterfind('.//{%s}subject' % NS['dc']):
+            print(subject.text)
     
 def pullDC(filename):
-    print("dc selected")    
+    print("dc selected, but not fully supported at the moment")    
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", help="input file")
-parser.add_argument("-m", "--metadataPrefix", help="select metadata encoding of source file", choices=["mods", "dc", "oai_dc"])
+parser.add_argument("file", help="input file")
+parser.add_argument("-m", "--metadataPrefix", help="select metadata encoding of source file", 
+                        choices=["mods", "dc", "oai_dc"], default="mods")
 args = parser.parse_args()
-fileName = args.file.split(".")[0]
+fileName = args.file
 if args.metadataPrefix == "mods":
     pullMODS(fileName)
 elif args.metadataPrefix == "oai_dc":
