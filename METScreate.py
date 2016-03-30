de455a7324cc927fe553a92baed4393cffaa5073
mrmiguez <r.m.miguez@gmail.com>
2016-03-30 12:44:38 -0400

    updating

--- METScreate.py
+++ METScreate.py
@@ -7,7 +7,7 @@ import hashlib
 import datetime
 import argparse
 
-def buildMETS(directory):
+def buildMETS(directory, agent_dict):
     NS = { 'mets' : 'http://www.loc.gov/METS/', 'mods': 'http://www.loc.gov/mods/v3' }
     with open(directory + '/' + directory + '.xml', 'w') as outFile:
         root = etree.Element("{%s}mets" % NS['mets'], OBJID=directory)
@@ -15,6 +15,14 @@ def buildMETS(directory):
                                     RECORDSTATUS="COMPLETE", 
                                     CREATEDATE=datetime.datetime.isoformat(datetime.datetime.now()),
                                     LASTMODDATE=datetime.datetime.isoformat(datetime.datetime.now()))
+        for type, individual in agent_dict.items():
+            agent = etree.SubElement(metsHdr, "{%s}agent" % NS['mets'], ROLE="CREATOR",
+                                        TYPE=type)
+            if agent['TYPE'] == 'OTHER':
+                agent['OTHERTYPE'] = "SOFTWARE"
+            name = etree.Element("{%s}name" % NS['mets'])
+            name.text = individual
+            agent.append(name)
         dmdSec = etree.SubElement(root, "{%s}dmdSec" % NS['mets'])
         fileSec = etree.SubElement(root, "{%s}fileSec" % NS['mets'])
         structMap = etree.SubElement(root, "{%s}structMap" % NS['mets'], 
@@ -30,8 +38,8 @@ def get_files(directory):
         
 agent_dict = { 'ORGANIZATION' : 'FSU,Florida State University', 'OTHER' : 'METScreate.py by FSU Libraries' }
 parser = argparse.ArgumentParser()
-parser.add_argument('-a', '--agent', help='FSUID of individual running this program')
 parser.add_argument('directory', help='directory containing files to be used in creating the METS document') 
+parser.add_argument('-a', '--agent', help='FSUID of the individual running this program')
 args = parser.parse_args()
-agent_dict['INDIVIDUAL'] = args.agent
-buildMETS(sys.argv[1])
+agent_dict['INDIVIDUAL'] = "FSU/" + args.agent
+buildMETS(args.directory, agent_dict)
