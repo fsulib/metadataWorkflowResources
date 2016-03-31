509e41aa3de0e392db742d504214709bfdf7ff6a
mrmiguez <r.m.miguez@gmail.com>
2016-03-31 15:24:14 -0400

    adding & moving MODS

--- METScreate.py
+++ METScreate.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
-from lxml import etree
-from functools import partial
 import os
 import sys
 import hashlib
 import datetime
 import argparse
+import mimetypes
+import shutil
+from lxml import etree
+from functools import partial
 
+#hashes image with MD5 hash
 def md5sum(filename):
     with open(filename, mode='rb') as f:
         d = hashlib.md5()
@@ -15,32 +18,45 @@ def md5sum(filename):
             d.update(buf)
     return d.hexdigest()
     
+#checks image file size
 def get_file_size(filename):
     statinfo = os.stat(filename)
     return statinfo.st_size
 
+#main function - builds & serializes the METS tree
 def buildMETS(directory, agent_dict):
+    print('Building METS for ' + directory + '.\n')
     NS = { 'mets' : 'http://www.loc.gov/METS/', 'mods': 'http://www.loc.gov/mods/v3',
-            'xlink' : 'http://www.w3.org/1999/xlink'}
-    with open(directory + '/' + 'mets.xml', 'w') as METSout:
+            'xlink' : 'http://www.w3.org/1999/xlink',
+            'xsi' : 'http://www.w3.org/2001/XMLSchema-instance' }
+    with open(directory + '.' + 'mets.xml', 'w') as METSout:
         root = etree.Element("{%s}mets" % NS['mets'], OBJID=directory)
         metsHdr = etree.SubElement(root, "{%s}metsHdr" % NS['mets'], 
                                     ID=directory,
                                     RECORDSTATUS="COMPLETE", 
                                     CREATEDATE=datetime.datetime.isoformat(datetime.datetime.now()),
                                     LASTMODDATE=datetime.datetime.isoformat(datetime.datetime.now()))
+        #add agents to metsHdr
         for type, individual in agent_dict.items():
             agent = etree.SubElement(metsHdr, "{%s}agent" % NS['mets'], 
                                     ROLE="CREATOR",
-                                    TYPE=type)
-# fix these two lines - interpretting str as int                                      
-#            if agent['TYPE'] == 'OTHER':
-#                agent['OTHERTYPE'] = "SOFTWARE"
+                                    TYPE=type)                                      
+            if agent.get('TYPE') == 'OTHER':
+                agent.set('OTHERTYPE', 'SOFTWARE')
             name = etree.Element("{%s}name" % NS['mets'])
             name.text = individual
             agent.append(name)
+        #copy in MODS descriptive metadata
         dmdSec = etree.SubElement(root, "{%s}dmdSec" % NS['mets'])
-        # copy MODS here
+        mdWrap = etree.SubElement(dmdSec, "{%s}mdWrap" % NS['mets'],
+                                    MDTYPE="MODS",
+                                    MIMETYPE="text/xml",
+                                    LABEL="MODS metadata")
+        xmlData = etree.SubElement(mdWrap, "{%s}xmlData" % NS['mets'])                                        
+        modsTree = etree.parse('MODS/' + directory + '.xml')
+        modsRoot = modsTree.getroot()
+        xmlData.append(modsRoot)
+        #build fileSec & structMap parents for iterative children 
         fileSec = etree.SubElement(root, "{%s}fileSec" % NS['mets'])
         fileGrp = etree.SubElement(fileSec, "{%s}fileGrp" % NS['mets'], 
                                     USE="archive")
@@ -56,12 +72,13 @@ def buildMETS(directory, agent_dict):
                                     ID="PDIV1",
                                     ORDER="1",
                                     TYPE="Main")                                     
+        #loop over files in directory & build fileSec & structMap children
         for image in os.listdir(directory):
             fileIndex = str(os.listdir(directory).index(image) + 1)
             file = etree.SubElement(fileGrp, "{%s}file" % NS['mets'], 
                                         GROUPID="G" + fileIndex,
                                         ID="TIF" + fileIndex,
-                                        MIMETYPE="image/tiff",
+                                        MIMETYPE=mimetypes.guess_type(image)[0],
                                         CHECKSUM=md5sum(directory + "/" + image),
                                         CHECKSUMTYPE="MD5",
                                         SIZE=str(get_file_size(directory + "/" + image)))
@@ -76,14 +93,19 @@ def buildMETS(directory, agent_dict):
                                         FILEID="TIF" + fileIndex)
             FLocat.set('{%s}href' % NS['xlink'], image)                            
 
-        #root.set('xmlns:METS', 'http://www.loc.gov/METS/')
-        METSout.write(etree.tostring(root, pretty_print=True).decode('utf-8'))
+        METSout.write(etree.tostring(root, pretty_print=True, xml_declaration=True).decode('utf-8'))
         
         
-agent_dict = { 'ORGANIZATION' : 'FSU,Florida State University', 'OTHER' : 'METScreate.py by FSU Libraries' }
+agent_dict = { 'ORGANIZATION' : 'FSU, Florida State University', 'OTHER' : 'METScreate.py by FSU Libraries' }
+
 parser = argparse.ArgumentParser()
 parser.add_argument('directory', help='directory containing files to be used in creating the METS document') 
 parser.add_argument('-a', '--agent', help='FSUID of the individual running this program')
 args = parser.parse_args()
 agent_dict['INDIVIDUAL'] = "FSU/" + args.agent
+
 buildMETS(args.directory, agent_dict)
+
+shutil.move(args.directory + '.mets.xml', args.directory + '/mets.xml')
+shutil.copy('MODS/' + args.directory + '.xml', args.directory + '/' + args.directory + '.xml')
+print(args.directory + ' fully packaged.\n')
