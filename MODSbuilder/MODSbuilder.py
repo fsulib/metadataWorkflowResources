04afd1f89793fc02eff620415597b921dc6d7b9b
mrmiguez <r.m.miguez@gmail.com>
2016-04-03 09:11:57 -0400

    new ideas for buildMods

--- MODSbuilder/MODSbuilder.py
+++ MODSbuilder/MODSbuilder.py
@@ -1,17 +1,34 @@
-from lxml import etree
+#!/usr/bin/env python3
+
 import csv
 import sys
+import argparse
+from lxml import etree
 
 def buildMODS(record):
-  root = etree.Element('mods')
-  for path, text in record.items():
-    print(path, text)
+    NS = { None : 'http://www.loc.gov/mods/v3',
+            'mods': 'http://www.loc.gov/mods/v3',
+            'flvc' : 'info:flvc/manifest',
+            'xlink' : 'http://www.w3.org/1999/xlink',
+            'xsi' : 'http://www.w3.org/2001/XMLSchema-instance' }
+    root = etree.Element('mods', nsmap=NS)
+    extension = etree.SubElement(root, 'extension')
+    flvc = etree.SubElement(extension, '{%s}flvc' % NS['flvc'])
+    owningInstitution = etree.SubElement(flvc, '{%s}owningInstitution' %NS['flvc'])
+    owningInstitution.text = 'FSU'
+    submittingInstitution = etree.SubElement(flvc, '{%s}submittingInstitution' %NS['flvc'])
+    submittingInstitution.text = 'FSU'
+    print(etree.tostring(root).decode("UTF-8"))
+#    for path, text in record.items():
+#        print(path, text)
     
 def readCSV(fileIn):
-  with open(fileIn) as csvfile:
-    source = csv.DictReader(csvfile, delimiter=';', quotechar='"')
-    for row in source:
-      buildMODS(row) 
+    with open(fileIn) as csvfile:
+        source = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+        for row in source:
+            buildMODS(row) 
       
-name = sys.argv[1]
-readCSV(name)      
+parser = argparse.ArgumentParser(description="create Metadata Object Description Schema XML documents from CSV files")
+parser.add_argument('CSV', help='CSV to be converted to MDOS')
+args = parser.parse_args()
+readCSV(args.CSV)      
