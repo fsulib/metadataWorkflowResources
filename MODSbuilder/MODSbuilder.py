3ab1e27f077a6e55b7d299f81cca082f0a821a33
mrmiguez <r.m.miguez@gmail.com>
2016-04-04 08:50:46 -0400

    testing

--- MODSbuilder/MODSbuilder.py
+++ MODSbuilder/MODSbuilder.py
@@ -8,28 +8,29 @@ import datetime
 from lxml import etree
 
 def buildMODS(record):
-    NS = { None : 'http://www.loc.gov/mods/v3',
-            'mods': 'http://www.loc.gov/mods/v3',
-            'flvc' : 'info:flvc/manifest',
-            'xlink' : 'http://www.w3.org/1999/xlink',
-            'xsi' : 'http://www.w3.org/2001/XMLSchema-instance' }
-    root = etree.Element('mods', nsmap=NS)
-    extension = etree.SubElement(root, 'extension')
-    flvc = etree.SubElement(extension, '{%s}flvc' % NS['flvc'])
-    owningInstitution = etree.SubElement(flvc, '{%s}owningInstitution' %NS['flvc'])
-    owningInstitution.text = 'FSU'
-    submittingInstitution = etree.SubElement(flvc, '{%s}submittingInstitution' %NS['flvc'])
-    submittingInstitution.text = 'FSU'
-    recordInfo = etree.SubElement(root, 'recordInfo')
-    recordCreationDate = etree.SubElement(recordInfo, 'recordCreationDate')
-    recordCreationDate.text = record['Record creation date']
-    descriptionStandard = etree.SubElement(recordInfo, 'descriptionStandard')
-    descriptionStandard.text = record['Description Standard']
-    recordOrigin = etree.SubElement(recordInfo, 'recordOrigin')
-    recordOrigin.text = 'Exported from CSV by MODSbuilder.py from FSU Libraries by ' + os.getlogin() + ' on ' + datetime.datetime.isoformat(datetime.datetime.now())
-    print(etree.tostring(root, pretty_print=True, xml_declaration=True, encoding="UTF-8").decode("UTF-8"))
-#    for path, text in record.items():
-#        print(path, text)
+    with open('MODStemp/' + record['IID'] + '.xml', 'w') as modsOut:
+        NS = { None : 'http://www.loc.gov/mods/v3',
+                'mods': 'http://www.loc.gov/mods/v3',
+                'flvc' : 'info:flvc/manifest',
+                'xlink' : 'http://www.w3.org/1999/xlink',
+                'xsi' : 'http://www.w3.org/2001/XMLSchema-instance' }
+        root = etree.Element('mods', nsmap=NS)
+        extension = etree.SubElement(root, 'extension')
+        flvc = etree.SubElement(extension, '{%s}flvc' % NS['flvc'])
+        owningInstitution = etree.SubElement(flvc, '{%s}owningInstitution' %NS['flvc'])
+        owningInstitution.text = 'FSU'
+        submittingInstitution = etree.SubElement(flvc, '{%s}submittingInstitution' %NS['flvc'])
+        submittingInstitution.text = 'FSU'
+        recordInfo = etree.SubElement(root, 'recordInfo')
+        recordCreationDate = etree.SubElement(recordInfo, 'recordCreationDate')
+        recordCreationDate.text = record['Record creation date']
+        descriptionStandard = etree.SubElement(recordInfo, 'descriptionStandard')
+        descriptionStandard.text = record['Description Standard']
+        recordOrigin = etree.SubElement(recordInfo, 'recordOrigin')
+        recordOrigin.text = 'Exported from CSV by MODSbuilder.py from FSU Libraries by ' + os.getlogin() + ' on ' + datetime.datetime.isoformat(datetime.datetime.now())
+        modsOut.write(etree.tostring(root, pretty_print=True, xml_declaration=True, encoding="UTF-8").decode("UTF-8"))
+    #    for path, text in record.items():
+    #        print(path, text)
     
 def readCSV(fileIn):
     with open(fileIn) as csvfile:
