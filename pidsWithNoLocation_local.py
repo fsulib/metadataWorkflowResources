155b16b88d5705f1800c4bed1756c77ad3b803bb
mrmiguez <r.m.miguez@gmail.com>
2016-05-11 10:44:50 -0400

    new xsl & local Location query tools

---
+++ pidsWithNoLocation_local.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from lxml import etree
+import re
+import os
+import sys
+
+def fsudl_pid_search(mods_record, nameSpace_dict):
+    pid = re.compile('fsu:[0-9]*')
+    for identifier in mods_record.iterfind('.//{%s}identifier' % nameSpace_dict['mods']):
+        match = pid.search(identifier.text)
+        if match is not None:
+            return match.group()
+
+def analyse_for_locations(fileName, dir):
+    NS = {'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/', 'dc': 'http://purl.org/dc/elements/1.1/', 'mods': 'http://www.loc.gov/mods/v3', 'dcterms': 'http://purl.org/dc/terms'}
+    tree = etree.parse(os.curdir + '/' + dir + fileName)
+    root = tree.getroot()
+    if root.find('.//{%s}physicalLocation' %NS['mods'] ) is not None:
+        for location in root.iterfind('.//{%s}physicalLocation' %NS['mods'] ):
+            if len(location.getparent().attrib) > 0:
+                print(fileName + ' : ' + 
+                      fsudl_pid_search(tree, NS)
+                       + ' -- LOCATION IN PURL'
+                      )
+    elif root.find('.//{%s}physicalLocation' %NS['mods'] ) is None:
+        print(fileName + ' : ' + 
+              fsudl_pid_search(tree, NS)
+              + ' -- NO LOCATION'
+              )
+            
+for fileIn in os.listdir(sys.argv[1]):
+    if os.path.isfile(os.curdir + '/' + sys.argv[1] + fileIn):
+        analyse_for_locations(fileIn, sys.argv[1])
+print('\nDone')
---
+++ xslt/stripLocationFromPURLs.xsl
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns="http://www.loc.gov/mods/v3"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:flvc="info:flvc/manifest/v1"
+  xmlns:mods="http://www.loc.gov/mods/v3" xmlns:dcterms="http://purl.org/dc/terms/"
+  xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-4.xsd" exclude-result-prefixes="xs" version="1.0"> 
+  
+  <xsl:output encoding="UTF-8" method="xml"/>
+  
+  <!--    Template to change this:
+        
+        <location displayLabel="purl">
+            <physicalLocation>Florida State University Libraries, Special Collections</physicalLocation>
+            <url>http://purl.flvc.org/fcla/dt/2665306</url>
+        </location>
+    
+        To this:
+    
+        <location displayLabel="purl">
+            <url>http://purl.flvc.org/fcla/dt/1924932</url>
+        </location>
+    -->  
+  
+  <!-- Generic Identity Template -->
+  <xsl:template match="@*|node()">
+    <xsl:copy>
+      <xsl:apply-templates select="@*|node()"/>
+    </xsl:copy>
+  </xsl:template>
+  
+  <xsl:template match="mods:location[@displayLabel='purl']">
+    <mods:location displayLabel="purl">
+      <xsl:copy-of select="mods:url"/>  
+    </mods:location>
+  </xsl:template>
+</xsl:stylesheet>
