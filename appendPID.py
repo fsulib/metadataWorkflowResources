84ebb9b521d0efa08be42b07dffdcb2453480492
mrmiguez <r.m.miguez@gmail.com>
2016-10-10 13:28:35 -0400

    bug fixes

--- appendPID.py
+++ appendPID.py
@@ -22,28 +22,30 @@ with open(sys.argv[1] + "out", 'w') as out_file:
     root = tree.getroot()
     for record in root.iterfind('.//{%s}record' % nameSpace_default['oai_dc']):
         PID = oai_dc_pid_search(record)
+        print(PID) # testing feedback
         PID_present = 0
-        mods_root = record.find('.//{%s}mods' % nameSpace_default['mods'])
-        if mods_root.find('.//{%s}identifier' % nameSpace_default['mods']) is None:
-            new_identifier = etree.SubElement(mods_root, "{%s}identifier" % nameSpace_default['mods'], type="fedora")
-            new_identifier.text = PID
-            edit_record_count = edit_record_count + 1
-            pass 
-        elif mods_root.find('.//{%s}identifier' % nameSpace_default['mods']) is not None:
-            for identifier in mods_root.iterfind('.//{%s}identifier' % nameSpace_default['mods']):
-                if len(identifier.attrib) == 0:
-                    pass
-                elif len(identifier.attrib) >= 1:
-                    if "type" in identifier.attrib.keys():
-                        if "fedora" in identifier.attrib["type"]:
-                            PID_present = 1
-            if PID_present != 1:
+        if record.find('.//{%s}mods' % nameSpace_default['mods']) is not None:
+            mods_root = record.find('.//{%s}mods' % nameSpace_default['mods'])
+            if mods_root.find('.//{%s}identifier' % nameSpace_default['mods']) is None:
                 new_identifier = etree.SubElement(mods_root, "{%s}identifier" % nameSpace_default['mods'], type="fedora")
                 new_identifier.text = PID
                 edit_record_count = edit_record_count + 1
-    out_file.write(etree.tostring(root, pretty_print=True,
-                                     xml_declaration=True,
-                                     encoding="UTF-8",
-                                     standalone=False).decode('utf-8'))
+                pass 
+            elif mods_root.find('.//{%s}identifier' % nameSpace_default['mods']) is not None:
+                for identifier in mods_root.iterfind('.//{%s}identifier' % nameSpace_default['mods']):
+                    if len(identifier.attrib) == 0:
+                        pass
+                    elif len(identifier.attrib) >= 1:
+                        if "type" in identifier.attrib.keys():
+                            if "fedora" in identifier.attrib["type"]:
+                                PID_present = 1
+                if PID_present != 1:
+                    new_identifier = etree.SubElement(mods_root, "{%s}identifier" % nameSpace_default['mods'], type="fedora")
+                    new_identifier.text = PID
+                    edit_record_count = edit_record_count + 1
+        out_file.write(etree.tostring(root, pretty_print=True,
+                                         xml_declaration=True,
+                                         encoding="UTF-8",
+                                         standalone=False).decode('utf-8'))
 print("\nComplete:\n\n" + str(edit_record_count), "records edited.\n")            
                 
