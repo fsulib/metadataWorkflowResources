15c9b186d7edfa039e266affccbea6df7539361a
mrmiguez <r.m.miguez@gmail.com>
2016-12-16 08:01:46 -0500

    workflow fix

--- renameByPID.py
+++ renameByPID.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python3
+
 import os
+import sys
 from lxml import etree
 
 NS = { 'mods':'http://www.loc.gov/mods/v3' }
 #files = os.listdir('MODS/')
-for xml in os.listdir('MODS/'):
+for xml in os.listdir(sys.argv[1]):
     print(xml)
-    tree = etree.parse('MODS/'+ xml)
+    tree = etree.parse(sys.argv[1]+ xml)
     root = tree.getroot()
     for element in root.iter("{%s}identifier" %NS['mods']):
         if element.get('type') is not None:
             if element.get('type') == 'fedora':
                 ID = element.text
-                tree.write('MODS/'+ ID.replace(':', '_') +'.xml', pretty_print=True, xml_declaration=True, encoding='UTF-8')
+                tree.write(sys.argv[1] + ID.replace(':', '_') +'.xml', pretty_print=True, xml_declaration=True, encoding='UTF-8')
